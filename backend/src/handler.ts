47e776a0cb3f2361bdcc802fb23064077cbff776
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-06-29 14:16:32 +0300

    init

---
+++ backend/src/handler.ts
@@ -0,0 +1,18 @@
+import type { APIGatewayProxyEvent} from "aws-lambda";
+
+module.exports.hello = async (event: APIGatewayProxyEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
