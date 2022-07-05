bb7dd8a9ceedb9b059f69ed71b8fde2f28c2ae33
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-07-05 23:08:40 +0300

    added get player function

---
+++ backend/src/Players/get.ts
@@ -0,0 +1,17 @@
+import type { APIGatewayProxyEvent } from "aws-lambda";
+import { getUser } from "../docClient";
+import { envelop } from "../envelop";
+
+module.exports.handler = async (event: APIGatewayProxyEvent) => {
+  let playerName = event.pathParameters?.player;
+  if (playerName) {
+    const user = await getUser(playerName);
+    if (user)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(user),
+      };
+    return envelop(`Player not found`, 404);
+  }
+  return envelop(`Player not found`, 404);
+};
